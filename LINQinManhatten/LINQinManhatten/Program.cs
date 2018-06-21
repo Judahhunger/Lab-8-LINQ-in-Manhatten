ba7e5a8d593941db85f387b69b0cc43a46829b59
Judah Hunger <judah.hunger@gmail.com>
2018-06-21 09:02:35 -0700

    initial push

---
+++ LINQinManhatten/LINQinManhatten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQinManhatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
